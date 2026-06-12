66c38accd7586d493636f22e6d1a9bbe1258ba17
denzang <dpak@ostriz.com>
2026-06-12 18:29:43 +0530

    Conditional Operators

--- part2/index.js
+++ part2/index.js
@@ -1,24 +1,15 @@
-// Push Pop Unshift Shift Challenge
-let largeCountries = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"];
+// Logical Operators
 
-// You need to help me fixup the largeCountries array so that
-// China and Pakistan are added back into their respective places
+let dayOfMonth = 13;
+let weekday = "Friday";
 
-// Use push() & pop() and their counterparts unshift() & shift()
-// Google how to use unshift() and shift()
+// If it is Friday the 13th, log out this spooky face: 😱
+// Use the logical "AND operator" -> &&
 
-// Removing Monaco using pop
-largeCountries.pop();
-console.log(`Array after removing Monaco: ${largeCountries}`);
+function spoookyFunction() {
+  if (dayOfMonth === 13 && weekday === "Friday") {
+    console.log(`😱`);
+  }
+}
 
-// Shift removes first element from the arrray
-largeCountries.shift();
-console.log(`Array after shifting Tuvalu: ${largeCountries}`);
-
-// Push adds an element to the last of the array
-largeCountries.push("Pakistan");
-console.log(`Array after pushing Pakistan: ${largeCountries}`);
-
-// Unshift is used to add elements to the beginning of the array
-largeCountries.unshift("China");
-console.log(`Array after unshifting China: ${largeCountries}`);
+spoookyFunction();
